8662444ffb8b70e0ea57f15817048033c409ec2c
Denix <denics@free.fr>
2015-07-23 18:59:17 +0200

    Update zz_carousel.pl
    Added custom size for images "cover"

--- cfg/cfg.d/zz_carousel.pl
+++ cfg/cfg.d/zz_carousel.pl
@@ -1,4 +1,4 @@
-
+# Add the field "carousel featured" to the eprint
 $c->add_dataset_field( "eprint",
 	{
 		name => "carousel_featured",
@@ -6,3 +6,14 @@ $c->add_dataset_field( "eprint",
 		volatile => 1,
 	}
 );
+
+# Change this value to change the size of your thumbnail
+$c->{plugins}->{'Convert::Thumbnails'}->{params}->{sizes} = {(
+    carousel => [980,450],
+)};
+
+# Add new size to list of files to be generated
+$c->{thumbnail_types} = sub {
+        my( $list, $repo, $doc ) = @_;
+        push @$list, qw( carousel ); 
+};
